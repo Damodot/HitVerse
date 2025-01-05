3de230fa91be8987899966c3cd7fdcc04fd961cd
damodotun <damodotunadedeji@gmail.com>
2025-01-05 19:07:40 +0100

    Third Deploy

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ let songListed = document.querySelector('.songList')
 let soundList = document.querySelector('.soundCheckList')
 let popList = document.querySelector('.popList')
 
+let myFirstView = document.querySelector('.loader');
+console.log(myFirstView);
+
+// Set initial display property
+myFirstView.style.display = 'flex';
+
+setTimeout(() => {
+    myFirstView.style.display = 'none'; // or any other value you want to change to after 3 seconds
+}, 3000);
 function songList() {
     if (myDisplay.style.display === '' || myDisplay.style.display === `block`) {
         myDisplay.style.display = `none`
